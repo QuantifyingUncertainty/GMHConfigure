35e8133dbf0f95a511c2eb219ba408af464afc2b
Kris De Meyer <KrisDM@users.noreply.github.com>
2016-06-08 01:00:33 +0100

    Create file

---
+++ jupyter_notebook_config_template.py
@@ -0,0 +1,6 @@
+c.NotebookApp.ip = '*'
+c.NotebookApp.port = 8998
+c.NotebookApp.open_browser = False
+c.NotebookApp.keyfile = u'/home/ubuntu/.certificates/jupyterkey.pem'
+c.NotebookApp.certfile = u'/home/ubuntu/.certificates/jupytercert.pem'
+c.NotebookApp.password = u'sha2:PASSWORDHASH'

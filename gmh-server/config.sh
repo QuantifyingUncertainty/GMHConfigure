58a93af4a8c058c64a5f92b3bfd5124e8e860596
Kris De Meyer <KrisDM@users.noreply.github.com>
2016-08-11 00:16:25 +0200

    Fix problem in sed

--- gmh-server/config.sh
+++ gmh-server/config.sh
@@ -16,7 +16,8 @@ echo " "
 SCRIPTDIR=$( dirname $0 )
 JUPTEMPLATE=$SCRIPTDIR/jupyter_notebook_config_template.py
 JUPCONFIG=$HOME/.jupyter/jupyter_notebook_config.py
-sed "s/PASSWORDHASH/$(ipython -c 'from notebook.auth import passwd; print(passwd())')/" <$JUPTEMPLATE >$JUPCONFIG
+HASHVALUE=$(ipython -c 'from notebook.auth import passwd; print(passwd())')
+sed "s@PASSWORDHASH@$HASHVALUE@" <$JUPTEMPLATE >$JUPCONFIG
 
 if [ -e $JUPCONFIG ]; then
     echo "Jupyter configuration file successfully created"
